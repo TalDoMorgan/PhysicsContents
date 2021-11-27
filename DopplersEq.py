604f4cc11306e4070624ba4be4c82519a5151355
TalDoMorgan <95113161+TalDoMorgan@users.noreply.github.com>
2021-11-27 00:09:35 -0300

    Add files via upload

---
+++ DopplersEq.py
@@ -0,0 +1,21 @@
+"""
+Created on Sat Nov 27 00:07:19 2021
+
+@author: Morgan
+"""
+
+print("Legenda:")
+print("V = velocidade da onda no meio;")
+print("Vo = velocidade do objeto em relacao à fonte;")
+print("Vs = velocidade da fonte em relacao ao meio;")
+print("FreqS = frequencia da fonte\n")
+print("Equacao: FreqS * ((V+Vo) / (V-Vs))")
+      
+V = input("Insira aqui a velocidade da onda (m/s): \n")
+Vo = input("Insira aqui a velocidade do observador (m/s): \n")
+Vs = input("Insira aqui a velocidade da fonte (m/s): \n")
+FreqS = input("Insira aqui a frequencia da fonte (Hz): \n")
+
+Eq = float(FreqS) * ((float(V)+float(Vo)) / (float(V)-float(Vs)))
+
+print("Frequencia observada =",Eq,"Hz")
